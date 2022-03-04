f25ea81796f323a3be64e0e26ee2e832875ec483
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-04 18:33:41 -0300

    fix keyboard hides textfield on Login and Register

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -7,81 +7,86 @@ class RegisterScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 212, 232, 231),
-      resizeToAvoidBottomInset: false,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset('lib/assets/images/logo.png'),
-          Card(
-            margin: const EdgeInsets.only(
-              left: 24,
-              right: 24,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(18, 48, 18, 18),
-              child: Column(
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('lib/assets/images/logo.png'),
+              Card(
+                margin: const EdgeInsets.only(
+                  left: 24,
+                  right: 24,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(18, 48, 18, 18),
+                  child: Column(
+                    children: [
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Nome',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Sobrenome',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      const TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Email',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      const TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          labelText: 'Senha',
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 12,
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          textStyle: const TextStyle(),
+                        ),
+                        onPressed: () {},
+                        child: const Text('REGISTRAR'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Column(
                 children: [
-                  const TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Nome',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Sobrenome',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const TextField(
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  const TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Senha',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      textStyle: const TextStyle(),
-                    ),
-                    onPressed: () {},
-                    child: const Text('REGISTRAR'),
+                  const Text('Já possui uma conta?'),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('FAZER LOGIN'),
                   ),
                 ],
               ),
-            ),
-          ),
-          Column(
-            children: [
-              const Text('Já possui uma conta?'),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Text('FAZER LOGIN'),
-              ),
             ],
           ),
-        ],
+        ),
       ),
     );
   }
