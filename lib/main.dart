8bb5cab823197b771b638f953791f91ffe2d5404
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-05 18:03:33 -0300

    rename the app name

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: '4grorice',
+      title: '4groRice',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       initialRoute: '/login',
