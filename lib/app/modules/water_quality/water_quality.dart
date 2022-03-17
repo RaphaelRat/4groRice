bc552f5d9a37894cf0be155025c757ec1b1a5d1b
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-17 16:09:37 -0300

    add responsiveContainer on water_quality page

--- lib/app/modules/water_quality/water_quality.dart
+++ lib/app/modules/water_quality/water_quality.dart
@@ -1,3 +1,5 @@
+import 'package:agrorice/app/global_widgets/global_widgets.dart';
+import 'package:agrorice/app/modules/home/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -19,141 +21,119 @@ class WaterQualityScreen extends StatelessWidget {
           return false;
         },
         child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // const SizedBox(
-              //   height: 60,
-              // ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.start,
-              //   crossAxisAlignment: CrossAxisAlignment.center,
-              //   children: [
-              //     IconButton(
-              //       icon: const Icon(
-              //         Icons.arrow_back,
-              //         color: Color.fromARGB(255, 65, 112, 110),
-              //       ),
-              //       onPressed: () => Get.back(),
-              //     ),
-              //     const Text(
-              //       'VOLTAR',
-              //       style: TextStyle(
-              //         color: Color.fromARGB(255, 65, 112, 110),
-              //         fontSize: 14,
-              //         fontWeight: FontWeight.w700,
-              //       ),
-              //       textAlign: TextAlign.start,
-              //     ),
-              //   ],
-              // ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    SizedBox(
-                      height: 24,
-                    ),
-                    Text(
-                      'Índices de Qualidade da Água',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 65, 112, 110),
-                        fontSize: 24,
-                        fontWeight: FontWeight.w600,
+          child: responsiveContainer(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _webAppBar(context),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      SizedBox(
+                        height: 24,
                       ),
-                      textAlign: TextAlign.start,
-                    ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                    Text(
-                      'pH: mede se uma solução é ácida ou basica.',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 102, 102, 102),
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
+                      Text(
+                        'Índices de Qualidade da Água',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 65, 112, 110),
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign: TextAlign.start,
                       ),
-                      textAlign: TextAlign.start,
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      'Oxigênio Dissolvido: águas poluídas, a uma baixa concentração de oxigênio dissolvido, enquanto que as águas limpas apresentam concentrações de oxigênio dissolvido elevadas, o que se torna um parâmetro de fácil acompanhamento da poluição da água.',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 102, 102, 102),
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
+                      SizedBox(
+                        height: 24,
                       ),
-                      textAlign: TextAlign.start,
-                    ),
-                    SizedBox(
-                      height: 12,
-                    ),
-                    Text(
-                      'Turbidez: nível da atenuação de intensidade que um feixe de luz sofre ao atravessá-la.',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 102, 102, 102),
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600,
+                      Text(
+                        'pH: mede se uma solução é ácida ou basica.',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 102, 102, 102),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign: TextAlign.start,
                       ),
-                      textAlign: TextAlign.start,
-                    ),
-                    SizedBox(
-                      height: 24,
-                    ),
-                  ],
-                ),
-              ),
-              const Divider(
-                color: Color.fromARGB(255, 212, 232, 231),
-                thickness: 1,
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                child: Text(
-                  'É importante o acompanhamento dos indices de qualidade da agua, pois muitos deles podem afetas a qualidade do arroz, além de que podem afetar a qualidade da água que consumida de outras formas pela população. Para realizar o acompanhamento, pode-se realizar testes em laboratórios da região, comprar o equipamentos prontos ou ainda construir um sistema de sensores.',
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 102, 102, 102),
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        'Oxigênio Dissolvido: águas poluídas, a uma baixa concentração de oxigênio dissolvido, enquanto que as águas limpas apresentam concentrações de oxigênio dissolvido elevadas, o que se torna um parâmetro de fácil acompanhamento da poluição da água.',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 102, 102, 102),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                      SizedBox(
+                        height: 12,
+                      ),
+                      Text(
+                        'Turbidez: nível da atenuação de intensidade que um feixe de luz sofre ao atravessá-la.',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 102, 102, 102),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                        ),
+                        textAlign: TextAlign.start,
+                      ),
+                      SizedBox(
+                        height: 24,
+                      ),
+                    ],
                   ),
-                  textAlign: TextAlign.start,
                 ),
-              ),
-              const SizedBox(
-                height: 24,
-              ),
-              const Divider(
-                color: Color.fromARGB(255, 212, 232, 231),
-                thickness: 1,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(left: 12, right: 12, top: 20),
-                child: Text(
-                  'Para saber mais sobre qualidade da água:',
-                  style: TextStyle(
-                    color: Color.fromARGB(153, 0, 0, 0),
-                    fontSize: 14,
-                    fontWeight: FontWeight.w600,
+                const Divider(
+                  color: Color.fromARGB(255, 212, 232, 231),
+                  thickness: 1,
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Text(
+                    'É importante o acompanhamento dos indices de qualidade da agua, pois muitos deles podem afetas a qualidade do arroz, além de que podem afetar a qualidade da água que consumida de outras formas pela população. Para realizar o acompanhamento, pode-se realizar testes em laboratórios da região, comprar o equipamentos prontos ou ainda construir um sistema de sensores.',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 102, 102, 102),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.start,
                   ),
-                  textAlign: TextAlign.center,
                 ),
-              ),
-              TextButton(
-                onPressed: () => _launchURL(),
-                child: const Text(
-                  'SABER MAIS',
-                  style: TextStyle(color: Color.fromARGB(255, 65, 112, 110), fontSize: 14, fontWeight: FontWeight.w500),
+                const SizedBox(
+                  height: 24,
                 ),
-              ),
-              const SizedBox(height: 15),
-            ],
+                const Divider(
+                  color: Color.fromARGB(255, 212, 232, 231),
+                  thickness: 1,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(left: 12, right: 12, top: 20),
+                  child: Text(
+                    'Para saber mais sobre qualidade da água:',
+                    style: TextStyle(
+                      color: Color.fromARGB(153, 0, 0, 0),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () => _launchURL(),
+                  child: const Text(
+                    'SABER MAIS',
+                    style: TextStyle(color: Color.fromARGB(255, 65, 112, 110), fontSize: 14, fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const SizedBox(height: 15),
+              ],
+            ),
           ),
         ),
       ),
@@ -161,36 +141,71 @@ class WaterQualityScreen extends StatelessWidget {
   }
 
   AppBar? _myAppBar(BuildContext context) {
-    return
-        //  MediaQuery.of(context).size.width >= 720
-        //     ? null
-        //     :
-        AppBar(
-      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-      elevation: 0,
-      leadingWidth: 112,
-      leading: Padding(
-        padding: const EdgeInsets.only(left: 8.0, top: 8),
-        child: GestureDetector(
-          onTap: Get.back,
-          child: Row(
-            children: const [
-              Icon(
-                Icons.arrow_back,
-                size: 24,
-                color: Color.fromARGB(255, 65, 112, 110),
+    return MediaQuery.of(context).size.width >= 720
+        ? null
+        : AppBar(
+            backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+            elevation: 0,
+            leadingWidth: 112,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 8.0, top: 8),
+              child: GestureDetector(
+                onTap: Get.back,
+                child: Row(
+                  children: const [
+                    Icon(
+                      Icons.arrow_back,
+                      size: 24,
+                      color: Color.fromARGB(255, 65, 112, 110),
+                    ),
+                    SizedBox(width: 6),
+                    Text(
+                      'Voltar',
+                      style: TextStyle(color: Color.fromARGB(255, 65, 112, 110), fontWeight: FontWeight.w500, letterSpacing: 1.1),
+                    )
+                  ],
+                ),
               ),
-              SizedBox(width: 6),
-              Text(
-                'Voltar',
-                style: TextStyle(color: Color.fromARGB(255, 65, 112, 110), fontWeight: FontWeight.w500, letterSpacing: 1.1),
-              )
+            ),
+          );
+  }
+}
+
+Widget _webAppBar(BuildContext context) {
+  return MediaQuery.of(context).size.width < 720
+      ? Container()
+      : responsiveContainer(
+          child: Padding(
+          padding: const EdgeInsets.only(top: 20, bottom: 60),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 8.0, top: 8),
+                child: GestureDetector(
+                  onTap: () {
+                    // Get.previousRoute.isEmpty ? Get.toNamed(HomeScreen.route) : Get.back();
+                    Get.previousRoute == HomeScreen.route ? Get.back() : Get.offNamed(HomeScreen.route);
+                  },
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.arrow_back,
+                        size: 24,
+                        color: Color.fromARGB(255, 65, 112, 110),
+                      ),
+                      SizedBox(width: 6),
+                      Text(
+                        'Voltar',
+                        style: TextStyle(color: Color.fromARGB(255, 65, 112, 110), fontWeight: FontWeight.w500, letterSpacing: 1.1),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
-        ),
-      ),
-    );
-  }
+        ));
 }
 
 _launchURL() async {
