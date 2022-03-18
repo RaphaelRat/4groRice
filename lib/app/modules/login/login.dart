c67342dc291abccff1993fde25d88e4ffb289088
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-18 13:26:07 -0300

    remove textfield focus on login page when tap out of field

--- lib/app/modules/login/login.dart
+++ lib/app/modules/login/login.dart
@@ -15,77 +15,80 @@ class LoginScreen extends GetView<LoginController> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 212, 232, 231),
-      body: SingleChildScrollView(
-        child: responsiveContainer(
-          minHeight: MediaQuery.of(context).size.height,
-          maxWidth: 420,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset('assets/images/logo.png'),
-              Card(
-                margin: const EdgeInsets.only(
-                  left: 24,
-                  right: 24,
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(18, 48, 18, 18),
-                  child: Column(
-                    children: [
-                      TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        controller: controller.emailController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Email',
+      body: GestureDetector(
+        onTap: Get.focusScope?.unfocus,
+        child: SingleChildScrollView(
+          child: responsiveContainer(
+            minHeight: MediaQuery.of(context).size.height,
+            maxWidth: 420,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset('assets/images/logo.png'),
+                Card(
+                  margin: const EdgeInsets.only(
+                    left: 24,
+                    right: 24,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(18, 48, 18, 18),
+                    child: Column(
+                      children: [
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: controller.emailController,
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                          ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Obx(
-                        () => TextField(
-                          keyboardType: TextInputType.visiblePassword,
-                          controller: controller.senhaController,
-                          obscureText: controller.isObscureText.value,
-                          decoration: InputDecoration(
-                            border: const OutlineInputBorder(),
-                            labelText: 'Senha',
-                            suffixIcon: IconButton(
-                              icon: Icon(
-                                controller.isObscureText.value ? Icons.visibility : Icons.visibility_off,
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        Obx(
+                          () => TextField(
+                            keyboardType: TextInputType.visiblePassword,
+                            controller: controller.senhaController,
+                            obscureText: controller.isObscureText.value,
+                            decoration: InputDecoration(
+                              border: const OutlineInputBorder(),
+                              labelText: 'Senha',
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  controller.isObscureText.value ? Icons.visibility : Icons.visibility_off,
+                                ),
+                                onPressed: () => controller.isObscureText.value = !controller.isObscureText.value,
                               ),
-                              onPressed: () => controller.isObscureText.value = !controller.isObscureText.value,
                             ),
                           ),
                         ),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          textStyle: const TextStyle(),
+                        const SizedBox(
+                          height: 12,
                         ),
-                        onPressed: controller.login,
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            textStyle: const TextStyle(),
+                          ),
+                          onPressed: controller.login,
 
-                        // onPressed: () => Get.offAllNamed(HomeScreen.route),
-                        child: const Text('ENTRAR'),
-                      ),
-                    ],
+                          // onPressed: () => Get.offAllNamed(HomeScreen.route),
+                          child: const Text('ENTRAR'),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Column(
-                children: [
-                  const Text('Não possui uma conta?'),
-                  TextButton(
-                    onPressed: () => Get.toNamed(RegisterScreen.route),
-                    child: const Text('REGISTRAR'),
-                  ),
-                ],
-              ),
-            ],
+                Column(
+                  children: [
+                    const Text('Não possui uma conta?'),
+                    TextButton(
+                      onPressed: () => Get.toNamed(RegisterScreen.route),
+                      child: const Text('REGISTRAR'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
