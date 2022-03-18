95f3065ffe9355291dc05d7aa2f9b56e00e39c29
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-17 21:25:47 -0300

    add UserSecureStorage

---
+++ lib/app/core/utils/user_secure_storage.dart
@@ -0,0 +1,45 @@
+import 'dart:convert';
+
+import 'package:agrorice/app/data/models/estimate.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class UserSecureStorage {
+  static const _storage = FlutterSecureStorage();
+
+  static const _keyUsername = 'username';
+  static const _keyEmail = 'email';
+  static const _keyJwt = 'token';
+  static const _keyEstimates = 'estimates';
+
+  static Future setUsername(String username) async => await _storage.write(key: _keyUsername, value: username);
+
+  static Future<String?> getUsername() async => await _storage.read(key: _keyUsername);
+
+  static Future setEmail(String email) async => await _storage.write(key: _keyEmail, value: email);
+
+  static Future<String?> getEmail() async => await _storage.read(key: _keyEmail);
+
+  static Future setjwt(String jwt) async => await _storage.write(key: _keyJwt, value: jwt);
+
+  static Future<String?> getjwt() async => await _storage.read(key: _keyJwt);
+
+  static Future setEstimates(List<Estimativa> estimates) async {
+    final value = json.encode(estimates);
+
+    await _storage.write(key: _keyEstimates, value: value);
+  }
+
+  static Future addEstimates(Estimativa estimate) async {
+    List<Estimativa>? estimates = await getEstimates();
+    estimates?.add(estimate);
+    final value = json.encode(estimates);
+
+    await _storage.write(key: _keyEstimates, value: value);
+  }
+
+  static Future<List<Estimativa>?> getEstimates() async {
+    final value = await _storage.read(key: _keyEstimates);
+
+    return value == null ? null : List<Estimativa>.from(json.decode(value));
+  }
+}
