f02e85bbd0d13c0c50ecd43f1e632e9688ce78f7
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-17 23:16:59 -0300

    add fields in HomeController onInit

--- lib/app/modules/home/home/home_controller.dart
+++ lib/app/modules/home/home/home_controller.dart
@@ -1,6 +1,21 @@
+import 'package:agrorice/app/core/utils/user_secure_storage.dart';
+import 'package:agrorice/app/data/models/estimate.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
   final solo = false.obs;
   final isObscureText = false.obs;
+  String? nome;
+  String? email;
+  String? _token;
+  List<Estimativa>? estimativas;
+
+  @override
+  Future<void> onInit() async {
+    nome = await UserSecureStorage.getUsername();
+    email = await UserSecureStorage.getEmail();
+    _token = await UserSecureStorage.getjwt();
+    estimativas = await UserSecureStorage.getEstimates();
+    super.onInit();
+  }
 }
