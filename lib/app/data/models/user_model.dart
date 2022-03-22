5eee50afbf2a05ddf620ff6599210983ba15d862
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-22 20:47:56 -0300

    define photo as optional field

--- lib/app/data/models/user_model.dart
+++ lib/app/data/models/user_model.dart
@@ -4,15 +4,15 @@ class User {
   String id;
   String name;
   String email;
-  String photo;
+  String? photo;
 
-  User({required this.id, required this.name, required this.email, required this.photo});
+  User({required this.id, required this.name, required this.email, this.photo});
 
   User.fromSnapshot(fire_auth.User currentUser)
       : id = currentUser.uid,
         name = currentUser.displayName ?? 'Sem nome',
         email = currentUser.email!,
-        photo = currentUser.photoURL ?? '0';
+        photo = currentUser.photoURL;
 
   Map<String, dynamic> toJson() {
     return {
@@ -25,6 +25,6 @@ class User {
 
   @override
   String toString() {
-    return 'id: $id, name: $name, email: $email, photo: $photo';
+    return 'User(id: $id, name: $name, email: $email, photo: $photo)';
   }
 }
