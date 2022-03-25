eb95c1bf6c954e27ac6f5d1680e468048e047747
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-25 00:59:01 -0300

    refactor estimate controller

--- lib/app/modules/home/home/home_controller.dart
+++ lib/app/modules/home/home/home_controller.dart
@@ -12,6 +12,68 @@ class HomeController extends GetxController {
   final estimativasStream = getEstimativasStream();
   final isEstimativasLoading = true.obs;
 
+  // Estimation controller
+  final vazaoController = TextEditingController();
+  final tempoPlantacaoController = TextEditingController();
+  final hectaresController = TextEditingController();
+  final tempoPreparacaoController = TextEditingController();
+
+  final dropdownValue = 'Sul'.obs;
+  final solo = false.obs;
+
+  void alteraRegiao(String regiao) {
+    dropdownValue.value = regiao;
+  }
+
+  void calcular() async {
+    Get.focusScope?.unfocus();
+
+    if (vazaoController.text.isEmpty ||
+        tempoPlantacaoController.text.isEmpty ||
+        hectaresController.text.isEmpty ||
+        dropdownValue.value.isEmpty ||
+        (solo.value && tempoPreparacaoController.text.isEmpty)) {
+      Get.defaultDialog(title: 'Erro', middleText: 'Preencha todos os campos!');
+      return;
+    }
+    double hectares;
+    int tempoPlantacao;
+    String regiao;
+    double vazao;
+    int preparacaoSolo;
+    try {
+      hectares = double.parse(hectaresController.text);
+      tempoPlantacao = int.parse(tempoPlantacaoController.text);
+      regiao = dropdownValue.value;
+      vazao = double.parse(vazaoController.text);
+      preparacaoSolo = solo.value ? int.parse(tempoPreparacaoController.text) : 0;
+    } catch (e) {
+      Get.defaultDialog(title: 'Erro', middleText: 'Algum campo está inválido!');
+      return;
+    }
+
+    final gastoDeAgua = Estimativa.calculaGasto(regiao: regiao, vazao: vazao, tempoPlantacao: tempoPlantacao, hectares: hectares);
+    Estimativa? estimativa;
+    try {
+      estimativa = Estimativa(
+          hectares: hectares, tempoPlantacao: tempoPlantacao, regiao: regiao, vazao: vazao, preparacaoSolo: preparacaoSolo, gastoDeAgua: gastoDeAgua);
+      addEstimativa(estimativa);
+    } catch (e) {
+      if (kDebugMode) {
+        print('\nERRO AO ENVIAR\n$e');
+      }
+      Get.defaultDialog(title: 'Erro', middleText: 'Erro ao enviar para o servidor');
+      return;
+    }
+
+    vazaoController.clear();
+    tempoPlantacaoController.clear();
+    hectaresController.clear();
+    tempoPreparacaoController.clear();
+
+    Get.to(() => EstimateResultScreen(estimativa: estimativa));
+  }
+
   // OnInit geral
   @override
   void onInit() async {
@@ -26,22 +88,10 @@ class HomeController extends GetxController {
   }
 
 //outros
-  final solo = false.obs;
-  final isObscureText = false.obs;
 
+  final isObscureText = false.obs;
   String? nome;
   String? email;
   String? _token;
   List<Estimativa>? estimativas;
-
-  final vazaoController = TextEditingController();
-  final tempoPlantacaoController = TextEditingController();
-  final hectaresController = TextEditingController();
-  final tempoPreparacaoController = TextEditingController();
-
-  final dropdownValue = 'Sul'.obs;
-
-  void alteraRegiao(String regiao) {
-    dropdownValue.value = regiao;
-  }
 }
