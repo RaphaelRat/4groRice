7a95b057ff67ae6e210909a926419d8cb478a251
Raphael Abreu <raphaelrat@hotmail.com>
2022-03-24 23:43:31 -0300

    remove unnecessary code in authentication repository

--- lib/app/data/repository/authentication.dart
+++ lib/app/data/repository/authentication.dart
@@ -4,23 +4,6 @@ import 'package:agrorice/app/data/providers/authentication/register.dart' as reg
 // import 'package:get/get.dart';
 
 class Authentication {
-  // Future<void> login2({required String email, required String senha}) async {
-  //   try {
-  //     UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(email: email, password: senha);
-
-  //     Get.defaultDialog(
-  //         title: 'FOI!', middleText: 'Nome:${userCredential.user!.displayName.toString()}\nEmail: ${userCredential.user!.email.toString()}');
-  //   } on FirebaseAuthException catch (e) {
-  //     if (e.code == 'user-not-found') {
-  //       Get.defaultDialog(title: "Erro", middleText: 'Nenhum usuário encontrado com esse e-mail.');
-  //     } else if (e.code == 'wrong-password') {
-  //       Get.defaultDialog(title: "Erro", middleText: 'Senha incorreta.');
-  //     }
-  //   } catch (e) {
-  //     Get.defaultDialog(title: 'Internal Erro', middleText: 'Erro interno, tente novamente em instantes');
-  //   }
-  // }
-
   static Future<String?> register({required String email, required String senha, required String nome}) async {
     try {
       await register_provider.register(email: email, senha: senha, nome: nome);
